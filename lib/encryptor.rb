65be4d299d418a499524de74a63965364bfd7681
Regina Casias <reginaannvigil@gmail.com>
2021-01-19 01:21:16 -0800

    Cleaned up encryptor

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -3,10 +3,10 @@ require './lib/enigma'
 class Encryptor
 
   def start
-    print "What would you like encrypted?"
+    print "What would you like to encrypt?"
     print "> "
     @description = $stdin.gets.chomp.downcase
-    print "Please wait while I create your random encryption code!"
+    puts "Please wait while I create your random encryption code!"
     sleep(2)
     create_code
   end
@@ -14,19 +14,18 @@ class Encryptor
   def create_code
     enigma = Enigma.new
     enigma.find_key_string
-    p enigma.create_key_array
-    p enigma.your_key
+    enigma.create_key_array
+    puts "Your decryption code is: #{enigma.your_key}"
+    puts "Please save this code for reference!"
     enigma.find_date_string
     enigma.create_date_array
     enigma.encrypt_array
-    p enigma.date
-
-
+    enigma.date
     enigma.alphabet
     enigma.encryption_cycle(@description)
     enigma.alphabet_numbers
     enigma.alphabet_rotation
     enigma.alphabet_rotation_encrypt(@description)
-    p enigma.find_letter_at_number_key(@description)
+    puts "Your encrypted message is: #{enigma.find_letter_at_number_key(@description)}"
   end
 end
